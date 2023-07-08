c4671808d9694fd35ed4494858329f26f070cfa9
Vendetta <hassandutra70@gmail.com>
2023-07-08 14:20:19 -0300

    mudança de planos

--- main.js
+++ main.js
@@ -82,16 +82,3 @@ btnCloseMenu.addEventListener('click', e => {
   })
   document.querySelector('body').style.overflow = 'visible'
 })
-
-import EmblaCarousel from 'embla-carousel'
-import '../css/base.css'
-import '../css/sandbox.css'
-import '../css/embla.css'
-
-const OPTIONS = { slidesToScroll: 'auto', containScroll: 'trimSnaps' }
-
-const emblaNode = document.querySelector('.embla')
-const viewportNode = emblaNode.querySelector('.embla__viewport')
-
-// @ts-ignore
-const emblaApi = EmblaCarousel(viewportNode, OPTIONS)
